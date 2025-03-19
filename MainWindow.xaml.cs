f88cc5d9e909b96d97f06edf4eb9d28b9055bee7
Roman <roman.ossif256@gmail.com>
2025-03-19 23:25:26 +0100

    Find a way to include images. Make code prettier.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,19 +1,7 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace MaimunasGift;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
     public MainWindow()
@@ -21,16 +9,9 @@ public partial class MainWindow : Window
         InitializeComponent();
     }
 
-    //private void Sleep()
-    //{
-    //    Thread.Sleep(4000);
-    //    //zzz...
-    //}
-
     private async void Button_Click(object sender, RoutedEventArgs e)
     {
-        Task.Delay(2000).Wait(); //it may be our hope, tho the app will be freezing. still...
-        //Sleep();
+        Task.Delay(2000).Wait();
 
         TestBlock1.Text = "Aha it has changed";
 
